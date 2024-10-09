1ed4342394a3d8fecd26e4ca275c35b6f71e5378
ifuensan <ivanfs@b4os.dev>
2024-10-09 12:24:39 +0200

    Refactor: Update docker-scripts.sh to exclude mempool/mysql/data directory

--- docker-scripts.sh
+++ docker-scripts.sh
@@ -74,9 +74,9 @@ cashu-regtest-start-log(){
 cashu-regtest-stop(){
   docker compose down --volumes
   # clean up lightning node data
-  sudo rm -rf ./data/clightning-1 ./data/clightning-2 ./data/clightning-3 ./data/lnd-1  ./data/lnd-2 ./data/lnd-3 ./data/boltz/boltz.db ./data/mempool-data
+  sudo rm -rf ./data/clightning-1 ./data/clightning-2 ./data/clightning-3 ./data/lnd-1  ./data/lnd-2 ./data/lnd-3 ./data/boltz/boltz.db ./data/mempool-data ./mempool/mysql/data/
   # recreate lightning node data folders preventing permission errors
-  mkdir ./data/clightning-1 ./data/clightning-2 ./data/clightning-3 ./data/lnd-1 ./data/lnd-2 ./data/lnd-3 ./data/mempool-data
+  mkdir ./data/clightning-1 ./data/clightning-2 ./data/clightning-3 ./data/lnd-1 ./data/lnd-2 ./data/lnd-3 ./data/mempool-data ./mempool/mysql/data/
 }
 
 cashu-regtest-restart(){
