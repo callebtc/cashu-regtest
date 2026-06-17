cf73ceb82139594ef592f1418f690b4c8aa403fe
a1denvalu3 <cloudsupper@proton.me>
2026-06-17 10:22:22 +0200

    Update CLN to 26.06.1 and LND to 0.21.0-beta

--- docker-scripts.sh
+++ docker-scripts.sh
@@ -74,9 +74,9 @@ cashu-regtest-start-log(){
 cashu-regtest-stop(){
   docker compose down --volumes
   # clean up lightning node data
-  sudo rm -rf ./data/clightning-1 ./data/clightning-2 ./data/clightning-3 ./data/lnd-1  ./data/lnd-2 ./data/lnd-3 ./data/boltz/boltz.db
+  docker run --rm -v "$(pwd)/data:/data" alpine rm -rf /data/clightning-1 /data/clightning-2 /data/clightning-3 /data/lnd-1 /data/lnd-2 /data/lnd-3 /data/boltz/boltz.db
   # recreate lightning node data folders preventing permission errors
-  mkdir ./data/clightning-1 ./data/clightning-2 ./data/clightning-3 ./data/lnd-1 ./data/lnd-2 ./data/lnd-3
+  mkdir -p ./data/clightning-1 ./data/clightning-2 ./data/clightning-3 ./data/lnd-1 ./data/lnd-2 ./data/lnd-3
 }
 
 cashu-regtest-restart(){
